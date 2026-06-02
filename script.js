200168916ae03296b56d97b2bf317b343d0c48b0
MD UMAR HUSSAIN <115338236+M-hussai@users.noreply.github.com>
2026-06-03 02:49:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-//your JS code here. If required.
-// Create the student object
 const student = {
   name: "John",
+  city:"dimapur"
 };
 
 // Function to get all keys from the object
-function getKeys(obj) {
-  return Object.keys(student);
+function getKeys() {
+  console.log(Object.keys(student));
 }
 
 // Testing the function
-console.log(getKeys(obj)); // Output: ["name"]
+getKeys(); // Output: ["name"]
